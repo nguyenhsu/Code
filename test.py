778638963dd4839a12d401de8ab6333c94a57d0b
SuNguyen <nguyenhuusu6793@gmail.com>
2020-06-18 00:00:55 +0700

    add function diagonal difference

--- test.py
+++ test.py
@@ -9,3 +9,12 @@ def simpleArraySum(ar):
 b =[1,2,3,4,5,6]
 print(simpleArraySum(b))
 
+
+def diagonalDifference(arr):
+    n = m = i = 0
+    while i < len(arr[0]):
+        n += arr[i][i]
+        m += arr[i][len(arr[0])-i-1]
+        i += 1
+    return abs(n - m)
+    # Write your code here
